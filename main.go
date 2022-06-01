a7cb5ecd31dcb233e7acb8b68c1bc2a5d06d8e41
Eric Slenk <lucid.machine@gmail.com>
2022-05-31 21:34:27 -0400

    Extract router setup

--- main.go
+++ main.go
@@ -5,8 +5,6 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/cors"
 )
 
 func getTodos(w http.ResponseWriter, r *http.Request) {
@@ -32,24 +30,8 @@ func deleteTodo(w http.ResponseWriter, r *http.Request) {
 func main() {
 	r := chi.NewRouter()
 
-	r.Use(middleware.Logger)
-	r.Use(middleware.CleanPath)
-	r.Use(middleware.RedirectSlashes)
-	r.Use(middleware.Timeout(5000))
-	r.Use(middleware.Compress(5))
-	r.Use(middleware.Recoverer)
-	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://*", "http://*"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: false,
-		MaxAge:           300,
-	}))
-
-	r.Get("/", getTodos)
-	r.Post("/", createTodo)
-	r.Delete("/", deleteTodo)
+	registerMiddleware(r)
+	registerRoutes(r)
 
 	http.ListenAndServe(":8080", r)
 }
---
+++ router.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+func registerMiddleware(r chi.Router) {
+	r.Use(middleware.Logger)
+	r.Use(middleware.CleanPath)
+	r.Use(middleware.RedirectSlashes)
+	r.Use(middleware.Timeout(5000))
+	r.Use(middleware.Compress(5))
+	r.Use(middleware.Recoverer)
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+}
+
+func registerRoutes(r chi.Router) {
+	r.Get("/", getTodos)
+	r.Post("/", createTodo)
+	r.Delete("/", deleteTodo)
+}
