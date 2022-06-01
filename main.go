a6ea73a8de268d9d2c950cd28c282844dedff03f
Eric Slenk <lucid.machine@gmail.com>
2022-05-31 21:59:17 -0400

    Extract controller

--- main.go
+++ main.go
@@ -1,31 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 )
-
-func getTodos(w http.ResponseWriter, r *http.Request) {
-	todos := []Todo{}
-	json.NewEncoder(w).Encode(todos)
-}
-
-func createTodo(w http.ResponseWriter, r *http.Request) {
-	var todo Todo
-	err := json.NewDecoder(r.Body).Decode(&todo)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-	}
-
-	w.WriteHeader(201)
-	json.NewEncoder(w).Encode(todo)
-}
-
-func deleteTodo(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(204)
-}
 
 func main() {
 	r := chi.NewRouter()
